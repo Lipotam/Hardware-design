800cbf8e8d8ac888e9b82f83a6a5b542c164f115
Lipotam <Lipotam@gmail.com>
2013-09-24 21:57:36 +0300

    lab2 base triggers added

---
+++ lab2/Lab2/JK_trigger_Test.udo
@@ -0,0 +1,2 @@
+## Project Navigator simulation template: JK_trigger_Test.udo
+## You may edit this file to control your simulation.
---
+++ lab2/Lab2/Up_Down_counter.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:38:25 09/24/2013 
+-- Design Name: 
+-- Module Name:    Up_Down_counter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Up_Down_counter is
+
+port (
+		DATA : in std_logic_vector(3 downto 0);
+		CDP, CPU, MR, notPL : in std_logic;
+      OUTPUT : out std_logic_vector(3 downto 0);
+		notTCD, notTCU : out std_logic
+	);
+
+end Up_Down_counter;
+
+architecture Behavioral of Up_Down_counter is
+
+begin
+
+
+end Behavioral;
+
