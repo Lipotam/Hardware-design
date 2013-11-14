0c26ed9f246823ba42fdfdd8d1287be0cad0caba
Lipotam <Lipotam@gmail.com>
2013-11-14 14:22:45 +0300

    second and third test fixed

--- lab4/Test_for_full_shema.vhd
+++ lab4/Test_for_full_shema.vhd
@@ -117,7 +117,7 @@ BEGIN
 	variable j : integer := 1; 
    begin		
       -- hold reset state for 100ms.
-     -- wait for 100ms;	
+      wait for 100ns;	
 
 --      wait for CLK_period*10;
 
@@ -138,9 +138,9 @@ BEGIN
 		j:=0;
 		while j < 10 loop
 			CLK_input <= '1';
-			wait for 1 ps;
+			wait for 10 ns;
 			CLK_input <= '0';
-			wait for 1 ps;
+			wait for 10 ns;
 			j := j + 1;
 		end loop;
 		
